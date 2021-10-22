8d1de0620ec6e93f08b86e4cbd40478ab652a6c7
Airene Maria <aireneapacible@gmail.com>
2021-10-22 22:20:57 +0800

    Unique category name

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ApplicationRecord
     has_many :tasks, dependent: :destroy
-    validates :title, presence: true, length: { maximum: 15 }
+    validates :title, presence: true, length: { maximum: 15 }, :uniqueness => true
     validates :body, presence: true, length: { minimum: 5 }
     validates :emoji, presence: true
 end
